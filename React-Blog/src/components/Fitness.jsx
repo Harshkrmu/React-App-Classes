15fdd796d818ef358b93be3bcb38eadaf8925e8f
Harshkrmu <harshescapematrix2008@gmail.com>
2026-04-22 15:49:11 +0530

    React Blog created

---
+++ React-Blog/src/components/Fitness.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Data from '../Data'
+
+const Fitness = () => {
+  const FitnessData = Data.filter((item) => item.category === "Fitness");
+  console.log(FitnessData);
+  return (
+    <div>
+      {FitnessData.map((fitnessdata) => (
+        <>
+          <h1>{fitnessdata.title}</h1>
+          <img src={fitnessdata.img_url}/>
+        </>
+      ))}
+    </div>
+  )
+}
+
+export default Fitness
